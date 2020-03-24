5c24fc99b8854966ed6d2b1de9c04a32dfae5562
Bernardo Santos <bsantospurchases@gmail.cok>
2020-03-24 15:21:24 +0000

    fix

--- Server.java
+++ Server.java
@@ -15,12 +15,8 @@ import java.security.spec.X509EncodedKeySpec;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Base64;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.Scanner;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -47,7 +43,7 @@ public class Server {
 
                 System.out.println("New User: " + serSocket);
 
-                ClientHandler tCli = new ClientHandler(serSocket, new Scanner(serSocket.getInputStream()), new PrintStream(serSocket.getOutputStream()), server.registeredUsers, server.generalBoard, server.usersSeeds, server.postCount);
+                ClientHandler tCli = server.new ClientHandler(serSocket, new Scanner(serSocket.getInputStream()), new PrintStream(serSocket.getOutputStream()));
                 tCli.start();
             }
             catch (Exception e) {
@@ -55,6 +51,168 @@ public class Server {
                 e.printStackTrace();
             }
         }   
+    }
+
+    class ClientHandler extends Thread {
+        Scanner dataIn;
+        PrintStream dataOut;
+        Socket s;
+    
+        public ClientHandler(Socket s, Scanner dataIn, PrintStream dataOut) {
+            this.s = s;
+            this.dataIn = dataIn;
+            this.dataOut = dataOut;
+        }
+    
+        @Override
+        public void run() {
+            int cliInput;
+            boolean actionLoop=true;
+    
+            while(actionLoop) {
+                try {
+                    cliInput = Integer.parseInt(this.dataIn.nextLine());
+                    switch (cliInput) {
+                        case 0:
+                            this.registerClient();
+                            break;
+                        case 1:
+                            this.post(true);
+                            break;
+                        case 2:
+                            this.post(false);
+                            break;
+                        case 3:
+                            this.read(true);
+                            break;
+                        case 4:
+                            this.read(false);
+                            break;
+                        case 5:
+                            actionLoop=false;
+                    }
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+    
+            try {
+                this.s.close();
+                this.dataIn.close();
+                this.dataOut.close();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    
+        public void registerClient() throws NoSuchAlgorithmException, InvalidKeyException, InvalidKeySpecException{
+            //Extract Public Key
+            PublicKey cliPublicKey = KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(Base64.getDecoder().decode(this.dataIn.nextLine())));
+            byte[] cliSeed = Base64.getDecoder().decode(this.dataIn.nextLine());
+            
+            //Init hashmaps with PublicKey obtained
+            ArrayList<Triplet> tmpTripletList = new ArrayList<Triplet>();
+            SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+            sr.setSeed(cliSeed);
+    
+            Server.this.usersSeeds.put(cliPublicKey, sr);
+            Server.this.registeredUsers.put(cliPublicKey, tmpTripletList);
+    
+            //Sucess
+            System.out.println("Client connected.");
+        }
+    
+        public void post(boolean boardToPost) throws InvalidKeyException, InvalidKeySpecException, NoSuchAlgorithmException, SignatureException{
+            //Extract arguments
+            PublicKey cliPublicKey = KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(Base64.getDecoder().decode(this.dataIn.nextLine())));
+            String message = this.dataIn.nextLine();
+    
+            String strRef = this.dataIn.nextLine();
+    
+            int[] references;
+            if (strRef.length() > 0) {
+                references = Arrays.stream(strRef.split(", ")).mapToInt(Integer::parseInt).toArray();
+            }
+            else {
+                references = new int[]{};
+            }
+    
+            byte hashedMessage[] = Base64.getDecoder().decode(this.dataIn.nextLine());
+    
+            //Verify nonce
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+    
+            byte nonce[] = new byte[20];
+            Server.this.usersSeeds.get(cliPublicKey).nextBytes(nonce);
+            md.update(nonce);
+    
+            byte verifyMessage[] = md.digest(message.getBytes());
+    
+            //Verify signature
+            Signature signature = Signature.getInstance("SHA1withRSA");
+            signature.initVerify(cliPublicKey);
+            signature.update(message.getBytes());
+            boolean verifySignature = signature.verify(Base64.getDecoder().decode(this.dataIn.nextLine()));
+    
+            //Check references validity
+            boolean refValidity = true;
+            for (int i=0; i<references.length; i++) {
+                if (references[i]>Server.this.postCount.get()) refValidity=false;
+            }
+    
+            System.out.println(refValidity);
+            System.out.println(verifySignature);
+    
+            if (Arrays.equals(hashedMessage, verifyMessage) && refValidity && verifySignature) {
+                if (boardToPost) {
+                    Triplet triplet = new Triplet(message, references, Server.this.postCount);
+                    Server.this.registeredUsers.get(cliPublicKey).add(triplet);
+                    System.out.println("Message posted to Announcement.");
+                }
+                else {
+                    Triplet triplet = new Triplet(message, references, cliPublicKey);
+                    Server.this.generalBoard.put(Server.this.postCount.get(), triplet);
+                    System.out.println("Message posted to general.");
+                }
+                Server.this.postCount.incrementAndGet();
+            }
+            else {
+                System.out.println("Message ignored.");
+            }
+        }
+    
+        public void read(boolean boardToRead) throws InvalidKeyException, InvalidKeySpecException, NoSuchAlgorithmException{
+            int postsToRead = Integer.parseInt(this.dataIn.nextLine());
+            if (postsToRead <= Server.this.postCount.get()) {
+                this.dataOut.println("1");
+                if (boardToRead) {
+                    PublicKey cliPublicKey = KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(Base64.getDecoder().decode(this.dataIn.nextLine())));
+                    ArrayList<Triplet> messageList = Server.this.registeredUsers.get(cliPublicKey);
+                    Collections.reverse(messageList);
+                    for(Triplet t : messageList) {
+                        if (postsToRead > 0) {
+                            this.dataOut.println(t.msg);
+                            postsToRead--;
+                        }
+                    }
+                }
+                else {
+                    ArrayList<Integer> tmpList = new ArrayList<Integer>(Server.this.generalBoard.keySet());
+                    int tmp = tmpList.size();
+                    while(postsToRead > 0) {
+                        this.dataOut.println(Server.this.generalBoard.get(tmpList.get(tmp-1)).msg);
+                        postsToRead--;
+                        tmp--;
+                    }
+                }
+            }
+            else {
+                this.dataOut.println("0");
+                System.out.println("Not enough posts to read.");
+            }
+        }
     }
 }
 
@@ -80,176 +238,5 @@ class Triplet {
 
     public void setId(Object id){
         this.id = id;
-    }
-}
-
-class ClientHandler extends Thread {
-    Scanner dataIn;
-    PrintStream dataOut;
-    Socket s;
-    ConcurrentHashMap<PublicKey, ArrayList<Triplet>> registeredUsers;
-    ConcurrentHashMap<Integer, Triplet> generalBoard;
-    ConcurrentHashMap<PublicKey, SecureRandom> usersSeeds;
-    AtomicInteger postCount;
-
-    public ClientHandler(Socket s, Scanner dataIn, PrintStream dataOut, ConcurrentHashMap<PublicKey, ArrayList<Triplet>> registeredUsers, ConcurrentHashMap<Integer, Triplet> generalBoard, ConcurrentHashMap<PublicKey, SecureRandom> usersSeeds, AtomicInteger postCount) {
-        this.s = s;
-        this.dataIn = dataIn;
-        this.dataOut = dataOut;
-        this.registeredUsers = registeredUsers;
-        this.generalBoard = generalBoard;
-        this.usersSeeds = usersSeeds;
-        this.postCount = postCount;
-    }
-
-    @Override
-    public void run() {
-        int cliInput;
-        String cliReceived;
-        boolean actionLoop=true;
-
-        while(actionLoop) {
-            try {
-                cliInput = Integer.parseInt(this.dataIn.nextLine());
-                switch (cliInput) {
-                    case 0:
-                        this.registerClient();
-                        break;
-                    case 1:
-                        this.post(true);
-                        break;
-                    case 2:
-                        this.post(false);
-                        break;
-                    case 3:
-                        this.read(true);
-                        break;
-                    case 4:
-                        this.read(false);
-                        break;
-                    case 5:
-                        actionLoop=false;
-                }
-            }
-            catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        try {
-            this.s.close();
-            this.dataIn.close();
-            this.dataOut.close();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void registerClient() throws NoSuchAlgorithmException, InvalidKeyException, InvalidKeySpecException{
-        //Extract Public Key
-        PublicKey cliPublicKey = KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(Base64.getDecoder().decode(this.dataIn.nextLine())));
-        byte[] cliSeed = Base64.getDecoder().decode(this.dataIn.nextLine());
-        
-        //Init hashmaps with PublicKey obtained
-        ArrayList<Triplet> tmpTripletList = new ArrayList<Triplet>();
-        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
-        sr.setSeed(cliSeed);
-
-        this.usersSeeds.put(cliPublicKey, sr);
-        this.registeredUsers.put(cliPublicKey, tmpTripletList);
-
-        //Sucess
-        System.out.println("Client connected.");
-    }
-
-    public void post(boolean boardToPost) throws InvalidKeyException, InvalidKeySpecException, NoSuchAlgorithmException, SignatureException{
-        //Extract arguments
-        PublicKey cliPublicKey = KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(Base64.getDecoder().decode(this.dataIn.nextLine())));
-        String message = this.dataIn.nextLine();
-
-        String strRef = this.dataIn.nextLine();
-
-        int[] references;
-        if (strRef.length() > 0) {
-            references = Arrays.stream(strRef.split(", ")).mapToInt(Integer::parseInt).toArray();
-        }
-        else {
-            references = new int[]{};
-        }
-
-        byte hashedMessage[] = Base64.getDecoder().decode(this.dataIn.nextLine());
-
-        //Verify nonce
-        MessageDigest md = MessageDigest.getInstance("SHA-1");
-
-        byte nonce[] = new byte[20];
-        usersSeeds.get(cliPublicKey).nextBytes(nonce);
-        md.update(nonce);
-
-        byte verifyMessage[] = md.digest(message.getBytes());
-
-        //Verify signature
-        Signature signature = Signature.getInstance("SHA1withRSA");
-        signature.initVerify(cliPublicKey);
-        signature.update(message.getBytes());
-        boolean verifySignature = signature.verify(Base64.getDecoder().decode(this.dataIn.nextLine()));
-
-        //Check references validity
-        boolean refValidity = true;
-        for (int i=0; i<references.length; i++) {
-            if (references[i]>postCount.get()) refValidity=false;
-        }
-
-        System.out.println(refValidity);
-        System.out.println(verifySignature);
-
-        if (Arrays.equals(hashedMessage, verifyMessage) && refValidity && verifySignature) {
-            if (boardToPost) {
-                Triplet triplet = new Triplet(message, references, postCount);
-                registeredUsers.get(cliPublicKey).add(triplet);
-                System.out.println("Message posted to Announcement.");
-            }
-            else {
-                Triplet triplet = new Triplet(message, references, cliPublicKey);
-                generalBoard.put(postCount.get(), triplet);
-                System.out.println("Message posted to general.");
-            }
-            this.postCount.incrementAndGet();
-        }
-        else {
-            System.out.println("Message ignored.");
-        }
-    }
-
-    public void read(boolean boardToRead) throws InvalidKeyException, InvalidKeySpecException, NoSuchAlgorithmException{
-        int postsToRead = Integer.parseInt(this.dataIn.nextLine());
-        if (postsToRead <= postCount.get()) {
-            this.dataOut.println("1");
-            if (boardToRead) {
-                PublicKey cliPublicKey = KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(Base64.getDecoder().decode(this.dataIn.nextLine())));
-                ArrayList<Triplet> messageList = registeredUsers.get(cliPublicKey);
-                Collections.reverse(messageList);
-                for(Triplet t : messageList) {
-                    if (postsToRead > 0) {
-                        this.dataOut.println(t.msg);
-                        postsToRead--;
-                    }
-                }
-            }
-            else {
-                ArrayList<Integer> tmpList = new ArrayList<Integer>(generalBoard.keySet());
-                int tmp = tmpList.size();
-                while(postsToRead > 0) {
-                    this.dataOut.println(generalBoard.get(tmpList.get(tmp-1)).msg);
-                    postsToRead--;
-                    tmp--;
-                }
-            }
-        }
-        else {
-            this.dataOut.println("0");
-            System.out.println("Not enough posts to read.");
-        }
     }
 }
